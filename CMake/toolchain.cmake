04458e660462151e741d3a9f211e3563d8fc873b
Amerlander <Amerlander@users.noreply.github.com>
2023-02-01 05:30:03 +0100

    Remove SoftDevice

--- CMake/toolchain.cmake
+++ CMake/toolchain.cmake
@@ -26,7 +26,7 @@ endif()
 set(MBED_LEGACY_TARGET_DEFINITIONS "NORDIC" "NRF51_CALLIOPE" "MCU_NRF51822" "MCU_NRF51_16K" "MCU_NORDIC_16K" "MCU_NRF51_16K_S110")
 # provide compatibility definitions for compiling with this target: these are
 # definitions that legacy code assumes will be defined.
-add_definitions("-DNRF51 -DTARGET_NORDIC -DTARGET_M0 -D__MBED__=1 -DMCU_NORDIC_16K -DTARGET_NRF51_CALLIOPE -DTARGET_MCU_NORDIC_16K -DTARGET_MCU_NRF51_16K_S110  -DTARGET_NRF_LFCLK_RC -DTARGET_MCU_NORDIC_16K -D__CORTEX_M0 -DARM_MATH_CM0")
+add_definitions("-DNRF51 -DTARGET_NORDIC -DTARGET_M0 -D__MBED__=1 -DMCU_NORDIC_16K -DTARGET_NRF51_CALLIOPE -DTARGET_MCU_NORDIC_16K -DTARGET_MCU_NRF51_16K_S110  -DTARGET_NRF_LFCLK_RC -DTARGET_MCU_NORDIC_16K -D__CORTEX_M0 -DARM_MATH_CM0 -DNO_BLE")
 
 # append non-generic flags, and set NRF51822-specific link script
 set(_CPU_COMPILATION_OPTIONS "-mcpu=cortex-m0 -mthumb -D__thumb2__ -fconserve-stack")
@@ -38,8 +38,8 @@ set(CMAKE_MODULE_LINKER_FLAGS_INIT "${CMAKE_MODULE_LINKER_FLAGS_INIT} -mcpu=cort
 set(CMAKE_EXE_LINKER_FLAGS_INIT    "${CMAKE_EXE_LINKER_FLAGS_INIT} -mcpu=cortex-m0 -mthumb -T\"${CMAKE_CURRENT_LIST_DIR}/../ld/NRF51822_${YOTTA_CFG_MICROBIT_DAL_RAM_SIZE}.ld\"")
 
 # used by the apply_target_rules function below:
-set(NRF51822_SOFTDEVICE_HEX_FILE "${CMAKE_CURRENT_LIST_DIR}/../softdevice/s110_nrf51822_8.0.0_softdevice.hex")
-set(NRF51822_BOOTLOADER_HEX_FILE "${CMAKE_CURRENT_LIST_DIR}/../bootloader/BLE_BOOTLOADER_RESERVED.hex")
+#set(NRF51822_SOFTDEVICE_HEX_FILE "${CMAKE_CURRENT_LIST_DIR}/../softdevice/s110_nrf51822_8.0.0_softdevice.hex")
+#set(NRF51822_BOOTLOADER_HEX_FILE "${CMAKE_CURRENT_LIST_DIR}/../bootloader/BLE_BOOTLOADER_RESERVED.hex")
 
 # define a function for yotta to apply target-specific rules to build products,
 # in our case we need to convert the built elf file to .hex, and add the
@@ -51,8 +51,9 @@ function(yotta_apply_target_rules target_type target_name)
             # objcopy to hex
             COMMAND arm-none-eabi-objcopy -O ihex ${target_name} ${target_name}.hex
             # and append the softdevice hex file
-            COMMAND srec_cat ${NRF51822_BOOTLOADER_HEX_FILE} -intel ${NRF51822_SOFTDEVICE_HEX_FILE} -intel ${target_name}.hex -intel -o ${target_name}-combined.hex -intel --line-length=44
-            COMMENT "hexifying and adding softdevice to ${target_name}"
+            #COMMAND srec_cat ${NRF51822_BOOTLOADER_HEX_FILE} -intel ${NRF51822_SOFTDEVICE_HEX_FILE} -intel ${target_name}.hex -intel -o ${target_name}-combined.hex -intel --line-length=44
+            COMMAND srec_cat ${target_name}.hex -intel -o ${target_name}.hex -intel --line-length=44
+            COMMENT "hexifying ${target_name}"
             VERBATIM
         )
     endif()
